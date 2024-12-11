7d78d293c57ecc875868233ec19c6e8ac3610d6c
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-12-10 21:43:00 -0300

    Update

---
+++ Build/IAzureSignTool.cs
@@ -0,0 +1,22 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tools.AzureSignTool;
+using Nuke.Common.Tools.AzureKeyVault;
+using ricaun.Nuke.Components;
+using ricaun.Nuke.Tools.NuGetKeyVaultSignTool;
+using Nuke.Common.Tools.ILRepack;
+using ricaun.Nuke.Tools;
+
+public interface IAzureSignTool : IClean, ICompile
+{
+    Target AzureSignTool => _ => _
+        .TriggeredBy(Clean)
+        .Before(Compile)
+        //.Requires<NuGetKeyVaultSignToolTasks>()
+        //.Requires<AzureSignToolTasks>()
+        .Requires<ILRepackTasks>()
+        .Executes(() =>
+        {
+            Serilog.Log.Information(AzureSignToolTasks.AzureSignToolPath);
+            Serilog.Log.Information(NuGetKeyVaultSignToolTasks.NuGetKeyVaultSignToolPath);
+        });
+}
